ed31afc36c113a6d8a1d5951b0adc0ad404c9eb5
GraturGud <torch-l1ght@yandex.ru>
2023-03-27 22:51:02 +0300

    more stuff

---
+++ sql-samples/codewars/reversed_strings.sql
@@ -0,0 +1,7 @@
+/*write your SQL statement here: you are given a table 'solution' with column 'str', 
+return a table with column 'str' and your result in a column named 'res'.*/
+SELECT 
+str, 
+reverse(str) as res 
+FROM 
+solution;
